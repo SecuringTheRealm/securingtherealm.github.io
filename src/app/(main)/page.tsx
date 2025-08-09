17d3c13c6286ae9ff9644f3a8f2c1677bd680421
Chris Lloyd-Jones <19361656+Sealjay@users.noreply.github.com>
2025-08-08 20:33:25 -0700

    style: format episode page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,59 +6,39 @@ import { MarkdownDescription } from '@/components/MarkdownDescription'
 import { YouTubeEmbed } from '@/components/YouTubeEmbed'
 import { type Episode, getAllEpisodes } from '@/lib/episodes'
 
-function PauseIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg aria-hidden="true" viewBox="0 0 10 10" {...props}>
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M1.496 0a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5H2.68a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5H1.496Zm5.82 0a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5H8.5a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5H7.316Z"
-      />
-    </svg>
-  )
-}
-
-function PlayIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg aria-hidden="true" viewBox="0 0 10 10" {...props}>
-      <path d="M8.25 4.567a.5.5 0 0 1 0 .866l-7.5 4.33A.5.5 0 0 1 0 9.33V.67A.5.5 0 0 1 .75.237l7.5 4.33Z" />
-    </svg>
-  )
-}
-
 function EpisodeEntry({ episode }: { episode: Episode }) {
-  let date = new Date(episode.published)
+  const date = new Date(episode.published)
 
   return (
     <article
       aria-labelledby={`episode-${episode.id}-title`}
-      className="py-10 sm:py-12"
+      className='py-10 sm:py-12'
     >
       <Container>
-        <div className="flex flex-col items-start">
+        <div className='flex flex-col items-start'>
           <h2
             id={`episode-${episode.id}-title`}
-            className="mt-2 text-lg font-bold text-slate-900"
+            className='mt-2 text-lg font-bold text-slate-900'
           >
-            <Link href={`${episode.url}`} target="_blank">
+            <Link href={`${episode.url}`} target='_blank'>
               {episode.title}
             </Link>
           </h2>
           <FormattedDate
             date={date}
-            className="order-first font-mono text-sm/7 text-slate-500"
+            className='order-first font-mono text-sm/7 text-slate-500'
           />
           <MarkdownDescription
             description={episode.description}
-            className="mt-1 text-base/7 text-slate-700"
+            className='mt-1 text-base/7 text-slate-700'
           />
 
           {/* YouTube Embed */}
-          <div className="mt-4 w-full max-w-2xl">
+          <div className='mt-4 w-full max-w-2xl'>
             <YouTubeEmbed
               url={episode.url}
               title={episode.title}
-              className="aspect-video"
+              className='aspect-video'
             />
           </div>
         </div>
@@ -68,14 +48,14 @@ function EpisodeEntry({ episode }: { episode: Episode }) {
 }
 
 export default async function Home() {
-  let episodes = await getAllEpisodes()
+  const episodes = await getAllEpisodes()
 
   return (
-    <div className="pt-16 pb-12 sm:pb-4 lg:pt-12">
+    <div className='pt-16 pb-12 sm:pb-4 lg:pt-12'>
       <Container>
-        <h1 className="text-2xl/7 font-bold text-slate-900">Episodes</h1>
+        <h1 className='text-2xl/7 font-bold text-slate-900'>Episodes</h1>
       </Container>
-      <div className="divide-y divide-slate-100 sm:mt-4 lg:mt-8 lg:border-t lg:border-slate-100">
+      <div className='divide-y divide-slate-100 sm:mt-4 lg:mt-8 lg:border-t lg:border-slate-100'>
         {episodes.map((episode) => (
           <EpisodeEntry key={episode.id} episode={episode} />
         ))}
